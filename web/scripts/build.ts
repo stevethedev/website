0084574a2f1bb3f49d97012d3602fefa35102441
Steven Jimenez <stevenmjimenez@gmail.com>
2025-01-20 13:47:21 -0500

    Install and configure React

--- web/scripts/build.ts
+++ web/scripts/build.ts
@@ -1,5 +1,5 @@
-import { readdir } from "node:fs/promises";
-import { join } from "node:path";
+import { copyFile, mkdir, readdir } from "node:fs/promises";
+import { join, basename, extname } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 import { build } from "esbuild";
 
@@ -9,23 +9,56 @@ main().catch((e) => {
 });
 
 async function main(): Promise<void> {
-  await build({
-    entryPoints: await getEntryPoints(),
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outdir: getFilePath("dist"),
-    platform: "browser",
-    target: "es2015",
+  const entryPoints = await getEntryPoints();
+
+  const buildPromises = entryPoints.map(async (entryPoint) => {
+    const appName = basename(entryPoint, extname(entryPoint));
+
+    await mkdir(getFilePath("dist", appName), { recursive: true });
+
+    await copyFiles(getFilePath("resource"), getFilePath("dist", appName));
+
+    await build({
+      entryPoints: [entryPoint],
+      bundle: true,
+      minify: true,
+      sourcemap: false,
+      outfile: getFilePath("dist", appName, "app.js"),
+      platform: "browser",
+      target: "es2015",
+    });
   });
+
+  await Promise.all(buildPromises);
 }
 
+/**
+ * Copy files from srcDir to destDir
+ * @param srcDir
+ * @param destDir
+ */
+async function copyFiles(srcDir: string, destDir: string): Promise<void> {
+  const files = await readdir(srcDir);
+  const copyPromises = files.map(async (file) => {
+    await copyFile(join(srcDir, file), join(destDir, file));
+  });
+
+  await Promise.all(copyPromises);
+}
+
+/**
+ * Get all entry points from src/entry-points
+ */
 async function getEntryPoints(): Promise<string[]> {
   const entryPointsDir = getFilePath("src", "entry-points");
   const entryPointsFiles = await readdir(entryPointsDir);
   return entryPointsFiles.map((file) => join(entryPointsDir, file));
 }
 
+/**
+ * Get file path from NPM project root
+ * @param fp
+ */
 function getFilePath(...fp: string[]): string {
   const baseUrl = new URL(join("..", ...fp), import.meta.url);
   return fileURLToPath(baseUrl);
---
+++ web/src/app/reader.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from "react";
+
+export default function Reader(): ReactNode {
+  return <div>Example Page</div>;
+}
